13d0ac3ace6a54d7a14e27558f11d47f99a4726f
Ballesd <ballesterosj500@gmail.com>
2022-06-26 13:41:03 -0500

    Reporte proveedor

--- Proyecto/resources/views/proveedor/generar_pdf.blade.php
+++ Proyecto/resources/views/proveedor/generar_pdf.blade.php
@@ -1,56 +1,50 @@
-@extends('layouts.app')
 
-@section('template_title')
-    Proveedor
-@endsection
-
-@section('content')
+<div>
     <div>
         <div>
             <div>
                 <div>
                     <div>
-                        <div>
 
-                            <span>
-                                {{ __('Proveedor') }}
-                            </span>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        
-										<th>Cedula</th>
-										<th>Nombre</th>
-										<th>Apellido</th>
-										<th>Telefono</th>
-										<th>Id Comp</th>
-
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($proveedor as $proveedor)
-                                        <tr>
-                                            
-											<td>{{ $proveedor->cedula }}</td>
-											<td>{{ $proveedor->nombre }}</td>
-											<td>{{ $proveedor->apellido }}</td>
-											<td>{{ $proveedor->telefono }}</td>
-											<td>{{ $proveedor->compañia->nombre_empresa }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                        <span>
+                            {{ __('Proveedor') }}
+                        </span>
                     </div>
                 </div>
+                <div>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    
+                                    <th>Cedula</th>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Telefono</th>
+                                    <th>Id Comp</th>
 
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($proveedor as $proveedor)
+                                    <tr>
+                                        
+                                        <td>{{ $proveedor->cedula }}</td>
+                                        <td>{{ $proveedor->nombre }}</td>
+                                        <td>{{ $proveedor->apellido }}</td>
+                                        <td>{{ $proveedor->telefono }}</td>
+                                        <td>{{ $proveedor->compañia->nombre_empresa }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
-@endsection
+</div>
+
