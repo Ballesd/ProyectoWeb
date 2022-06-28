1177aabcb52b52f8e9628d47e98c5b0e689ba09a
Danny <djvasquezce@unal.edu.co>
2022-06-27 21:18:35 -0500

    correcciones y rutas

--- Proyecto/resources/views/home.blade.php
+++ Proyecto/resources/views/home.blade.php
@@ -12,7 +12,7 @@
 
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('inventario.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-sky-300">
+            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">
                 <h1>Gestión de Inventario</h1>
             </div>
         </a>
@@ -20,36 +20,40 @@
 
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('proveedor.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-sky-300">
+            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">
                 <h1>Gestión de Proveedores</h1>
+                <div class="flex justify-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2103/2103848.png" alt="imagen de proveedor" class="h-36">
+                </div>
+
             </div>
         </a>
         @endif
 
         <a href="">
             <div class="h-60 grid grid-cols-2 grid-rows-2 gap-5 text-xl">
-                <div class="col-span-1 row-span-2 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-sky-300">Datos abiertos</div>
+                <div class="col-span-1 row-span-2 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">Datos abiertos</div>
 
-                <div class="col-span-1 row-span-1 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-sky-300">Más funciones</div>
-                <div class="col-span-1 row-span-1 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-sky-300">Atención al cliente</div>
+                <div class="col-span-1 row-span-1 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">Más funciones</div>
+                <div class="col-span-1 row-span-1 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">Atención al cliente</div>
             </div>
         </a>
 
         <a href="{{ route('empleado.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-sky-300">
-                <h1>Rcursos Humanos</h1>
+            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">
+                <h1>Recursos Humanos</h1>
             </div>
         </a>
-       
+
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('produccion.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-sky-300">
+            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500">
                 <h1>Gestión de producción</h1>
             </div>
         </a>
         @endif
 
-    
+
     </article>
 
 
