295e2bb64e487571213ee8dd4927bd192a990b35
Danny <djvasquezce@unal.edu.co>
2022-06-27 21:31:00 -0500

    corrección grid

--- Proyecto/resources/views/home.blade.php
+++ Proyecto/resources/views/home.blade.php
@@ -12,7 +12,7 @@
 
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('inventario.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
+            <div class="h-72 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
                 <h1>Gestión de Inventario</h1>
             </div>
         </a>
@@ -20,7 +20,7 @@
 
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('proveedor.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
+            <div class="h-72 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
                 <h1>Gestión de Proveedores</h1>
                 <div class="flex justify-center">
                     <img src="https://cdn-icons-png.flaticon.com/512/2103/2103848.png" alt="imagen de proveedor" class="h-36">
@@ -31,7 +31,7 @@
         @endif
 
         <a href="">
-            <div class="h-60 grid grid-cols-2 grid-rows-2 gap-5 text-xl">
+            <div class="h-72 grid grid-cols-2 grid-rows-2 gap-5 text-xl">
                 <div class="col-span-1 row-span-2 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">Datos abiertos</div>
 
                 <div class="col-span-1 row-span-1 rounded-2xl p-4 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">Más funciones</div>
@@ -40,14 +40,14 @@
         </a>
 
         <a href="{{ route('empleado.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
+            <div class="h-72 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
                 <h1>Recursos Humanos</h1>
             </div>
         </a>
 
         @if((auth()->user()->role) == 'Administrador')
         <a href="{{ route('produccion.index') }}">
-            <div class="h-60 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
+            <div class="h-72 rounded-2xl p-5 bg-white shadow-2xl flex flex-col hover:bg-gradient-to-r hover:from-sky-200 hover:to-sky-500 hover:border border-black">
                 <h1>Gestión de producción</h1>
             </div>
         </a>
