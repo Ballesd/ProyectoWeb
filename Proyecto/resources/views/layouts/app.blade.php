a02cd1e09cf96016ccdc2db796741d10824a35d0
Danny <djvasquezce@unal.edu.co>
2022-06-29 02:19:59 -0500

    corrección ventana

--- Proyecto/resources/views/layouts/app.blade.php
+++ Proyecto/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - laravel App</title>
+    <title>@yield('title') - PBV</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/cfe1e1aa1c.js" crossorigin="anonymous"></script>
     <script src="https://cdn.plot.ly/plotly-2.11.1.min.js"></script>
